9d53b66abdbd2e2b1797f0e6f14eca33c268e3ef
Johannes Westberg <Johannes.Westberg.0339@student.uu.se>
2021-01-18 20:51:38 +0100

    initial commit

---
+++ Pomodoro/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Pomodoro
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.autostartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.label = new System.Windows.Forms.Label();
+			this.contextMenuStrip.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notifyIcon
+			// 
+			this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+			this.notifyIcon.BalloonTipText = "Pause";
+			this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+			this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+			this.notifyIcon.Text = "Pomodoro";
+			this.notifyIcon.Visible = true;
+			this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+			// 
+			// contextMenuStrip
+			// 
+			this.contextMenuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+			this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.autostartToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+			this.contextMenuStrip.Name = "contextMenuStrip";
+			this.contextMenuStrip.Size = new System.Drawing.Size(211, 116);
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(210, 26);
+			this.openToolStripMenuItem.Text = "Open";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+			// 
+			// autostartToolStripMenuItem
+			// 
+			this.autostartToolStripMenuItem.Checked = true;
+			this.autostartToolStripMenuItem.CheckOnClick = true;
+			this.autostartToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.autostartToolStripMenuItem.Name = "autostartToolStripMenuItem";
+			this.autostartToolStripMenuItem.Size = new System.Drawing.Size(210, 26);
+			this.autostartToolStripMenuItem.Text = "Auto-start";
+			this.autostartToolStripMenuItem.CheckStateChanged += new System.EventHandler(this.AutostartToolStripMenuItem_CheckStateChanged);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(207, 6);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(210, 26);
+			this.exitToolStripMenuItem.Text = "Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// label
+			// 
+			this.label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label.AutoSize = true;
+			this.label.Font = new System.Drawing.Font("Malgun Gothic", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label.ForeColor = System.Drawing.Color.DarkGreen;
+			this.label.Location = new System.Drawing.Point(3, 46);
+			this.label.Name = "label";
+			this.label.Size = new System.Drawing.Size(228, 89);
+			this.label.TabIndex = 0;
+			this.label.Text = "label1";
+			this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.ClientSize = new System.Drawing.Size(247, 195);
+			this.Controls.Add(this.label);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.Name = "Form1";
+			this.Resize += new System.EventHandler(this.Form1_Resize);
+			this.contextMenuStrip.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIcon;
+		private System.Windows.Forms.Label label;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem autostartToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+	}
+}
+
---
+++ Pomodoro/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Pomodoro
+{
+	public partial class Form1 : Form
+	{
+		enum State { Break, Work }
+
+		const string RegistryName = "Johannes Pomodoro";
+
+		State currentState;
+		State CurrentState
+		{
+			get { return currentState; }
+			set
+			{
+				if (value != currentState)
+					_CurrentState = value;
+			}
+		}
+		State _CurrentState
+		{
+			get { return currentState; }
+			set
+			{
+				currentState = value;
+				label.Text = currentState.ToString();
+				notifyIcon.BalloonTipText = currentState.ToString();
+				notifyIcon.ShowBalloonTip(10_000);
+				
+				// test
+				this.WindowState = FormWindowState.Minimized;
+				this.Show();
+				this.WindowState = FormWindowState.Normal;
+			}
+		}
+
+		Timer timer = new Timer();
+
+		public Form1()
+		{
+			Func<State> checkState = () =>
+				//(DateTime.Now.Minute >= 55 && DateTime.Now.Minute <= 59) ||
+				//(DateTime.Now.Minute >= 25 && DateTime.Now.Minute <= 29) ?
+				(DateTime.Now.Minute >= 50 && DateTime.Now.Minute <= 59) ?
+					State.Break :
+					State.Work;
+
+			InitializeComponent();
+			timer.Interval = 1000;
+			timer.Tick += (obj, args) => CurrentState = checkState();
+			timer.Start();
+			label.Text = CurrentState.ToString();
+
+			_CurrentState = checkState();
+
+			using (var registry = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+			{
+				autostartToolStripMenuItem.Checked = registry.GetValue(RegistryName) != null;
+			}
+		}
+
+		private void Form1_Resize(object sender, EventArgs e)
+		{
+			if (WindowState == FormWindowState.Minimized)
+				Hide();
+		}
+
+		private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			Show();
+			WindowState = FormWindowState.Normal;
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void openToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Show();
+			WindowState = FormWindowState.Normal;
+		}
+
+		private void AutostartToolStripMenuItem_CheckStateChanged(object sender, EventArgs e)
+		{
+			using (var registry = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+			{
+				if (autostartToolStripMenuItem.Checked)
+					registry.SetValue(RegistryName, Application.ExecutablePath.ToString());
+				else
+					registry.DeleteValue(RegistryName, false);
+			}
+		}
+	}
+}
